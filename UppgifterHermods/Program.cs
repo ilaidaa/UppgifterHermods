9f39d42f08013a7a7aec4d911a11cca394542328
ilaidaa <ilaida.e@hotmail.com>
2025-10-13 18:29:47 +0200

    Add project files.

---
+++ UppgifterHermods/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace UppgifterHermods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uppgift 1: Pensionen
+            Console.WriteLine("Välkomna till denna pensionssimulator.\n");
+
+
+            // Inmatning och felhantering
+            Console.Write("Vad heter du i förnman? : ");
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Ogiltig inmatning. Försök igen. ");
+                Thread.Sleep(1000);
+                return;
+            }
+
+            Console.Write("Hur gammal är du? : ");
+           
+            if(!int.TryParse(Console.ReadLine(), out int age))
+            {
+                Console.WriteLine("Ogiltig inmatning. Försök igen. ");
+                Thread.Sleep(1000);
+                return;
+            }
+
+
+            // Pensions uträkning
+            int pensionAge = 65;
+            int yearsLeft = pensionAge - age;
+
+
+            // Output
+            if(yearsLeft > 0)
+            {
+                Console.WriteLine($"\nHej {name}. Du går i pension om {yearsLeft} år. ");
+            }
+            else
+            {
+                Console.WriteLine($"\nHej {name}. Du har redan passerat pensionsåldern!");
+            }
+
+
+            Console.WriteLine("\nPress any key to continue . . .");
+            Console.ReadKey(true);
+        }
+    }
+}
